58f855c7f737b84b23ecbb7b7058e68ccd1e4a68
QkeleQ10 <quintenra@gmail.com>
2021-04-22 14:39:09 +0200

    Create availableLanguages.sh

---
+++ availableLanguages.sh
@@ -0,0 +1,1 @@
+curl -X GET https://crowdin.com/api/v2/projects/428718/languages/progress -H "Authorization: Bearer 619136399475cf3a9457e59502df76a0c5f8ab401982954197fb501dab44f6668bed46b72fb123ae" -o availableLanguages.json
